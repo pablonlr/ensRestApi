f67ca2459649209e4cd75267fa8cf8cb44e2cc82
Pablo Lara <pablonlarag@gmail.com>
2022-09-21 00:47:44 -0400

    update server address

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 	"github.com/gorilla/mux"
 )
 
-const serverAddr = "127.0.0.1:8000"
+const serverAddr = ":8000"
 
 func createServer(router http.Handler, addr string, timeout time.Duration) *http.Server {
 	return &http.Server{
